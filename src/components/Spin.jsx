b4dedca648089218ae27ef097eb7f0a080df3175
Ben Matela <bmatela@afrogeek.co.za>
2022-08-16 15:02:26 +0200

    added attempts

--- src/components/Spin.jsx
+++ src/components/Spin.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Spin = ({ handleDrawClick }) => {
   const [generatedNumber, setGeneratedNumber] = useState(0);
+  const [attempts, setAttempts] = useState(0);
 
   const generatedRandomNumber = (min, max) => {
     return Math.round(min + Math.random() * (max - min));
@@ -11,6 +12,7 @@ const Spin = ({ handleDrawClick }) => {
     const random = generatedRandomNumber(1, 50);
     handleDrawClick(random);
     setGeneratedNumber(random);
+    setAttempts((previous) => (previous += 1));
   };
 
   return (
@@ -27,12 +29,16 @@ const Spin = ({ handleDrawClick }) => {
           onClick={handleGenerateRandom}>
           Spin!
         </button>
-        <p className="font-medium mx-auto mt-2 text-white">
+        <p className="font-medium mx-auto mt-3 text-white">Attempts:</p>
+        <h1 className="mx-auto text-white md:text-4xl sm:text-3xl text-2xl font-bold">
+          {attempts}
+        </h1>
+        <h3 className="font-bold mx-auto mt-4 text-[#00df9a]">
           Boxes will be popped if they match your draw
-        </p>
+        </h3>
       </div>
     </div>
   );
-}
+};
 
 export default Spin;
