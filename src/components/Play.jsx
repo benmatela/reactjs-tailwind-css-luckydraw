056f03ccaa45dfff6886909e5f1b7a83abcf2b2c
Ben Matela <bmatela@afrogeek.co.za>
2022-08-18 06:52:28 +0200

    disable spin when game over

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -23,7 +23,7 @@ export const Play = () => {
     <div className="w-full bg-black px-4 text-white">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
         <div className="flex">
-          <Spin handleDrawClick={handleDrawClick}  />
+          <Spin handleDrawClick={handleDrawClick} cards={cards}  />
         </div>
         <div id="cardsContainer">
           {cards.length === 0 ? (
--- src/components/Spin.jsx
+++ src/components/Spin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export const Spin = ({ handleDrawClick }) => {
+export const Spin = (props) => {
   const [generatedNumber, setGeneratedNumber] = useState(0);
   const [attempts, setAttempts] = useState(0);
 
@@ -10,7 +10,7 @@ export const Spin = ({ handleDrawClick }) => {
 
   const handleGenerateRandom = () => {
     const random = generatedRandomNumber(1, 50);
-    handleDrawClick(random);
+    props.handleDrawClick(random);
     setGeneratedNumber(random);
     setAttempts((previous) => (previous += 1));
   };
@@ -27,12 +27,18 @@ export const Spin = ({ handleDrawClick }) => {
         </h1>
         <button
           className="py-3 my-6 bg-[#00df9a] text-[#000300] w-[200px] rounded-md font-medium mx-auto"
-          onClick={handleGenerateRandom}>
+          onClick={handleGenerateRandom}
+          disabled={props.cards.length === 0}
+        >
           SPIN!
         </button>
         <p className="font-medium mx-auto mt-3 text-white">Attempts:</p>
         <h1 className="mx-auto text-white md:text-4xl sm:text-3xl text-2xl font-bold">
           {attempts}
+        </h1>
+        <p className="font-medium mx-auto mt-3 text-white">Remaining Cards:</p>
+        <h1 className="mx-auto text-white md:text-4xl sm:text-3xl text-2xl font-bold">
+          {props.cards.length}
         </h1>
         <h3 className="font-bold mx-auto mt-4 text-[#00df9a]">
           Boxes will be popped if they match your draw! 💰
